1e2e96e0b6ea2b630c01f45c16fdcce6d73fee23
Andrii <148819392+andriikamanin@users.noreply.github.com>
2025-03-03 15:34:44 +0100

    Добавления сгенерированного Spring Boot проекта

---
+++ EmergencyAlertAPI/src/main/java/it/volta/ts/kamaninandrii/EmergencyAlertAPI/EmergencyAlertApiApplication.java
@@ -0,0 +1,13 @@
+package it.volta.ts.kamaninandrii.EmergencyAlertAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyAlertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyAlertApiApplication.class, args);
+	}
+
+}
---
+++ EmergencyAlertAPI/src/test/java/it/volta/ts/kamaninandrii/EmergencyAlertAPI/EmergencyAlertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.volta.ts.kamaninandrii.EmergencyAlertAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyAlertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
